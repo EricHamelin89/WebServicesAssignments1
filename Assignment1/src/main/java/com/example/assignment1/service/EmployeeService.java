2db69a8a3e89f245d3592aedb963343234112a7a
EricHamelin89 <91698725+EricHamelin89@users.noreply.github.com>
2022-02-17 17:52:59 -0500

    Update EmployeeService.java
    Changed phone numbers (weren't showing up correctly)

--- Assignment1/src/main/java/com/example/assignment1/service/EmployeeService.java
+++ Assignment1/src/main/java/com/example/assignment1/service/EmployeeService.java
@@ -13,19 +13,19 @@ public class EmployeeService {
     public List<Employee> getAllEmployees() {
 
         Employee employee1 = new Employee(1, "Tony", "Sorpano", "Boss",
-                new Address(14, "Aspen", "Caldwell", 07061, "New Jersey", "United States"), 201 - 264 - 1111);
+                new Address(14, "Aspen", "Caldwell", 07061, "New Jersey", "United States"), 2012641111);
         Employee employee2 = new Employee(2, "Silvio", "Dante", "Consigliere",
-                new Address(14, "BadaBing Road", "Belleville", 07025, "New Jersey", "United States"), 201 - 254 - 2121);
+                new Address(14, "BadaBing Road", "Belleville", 07025, "New Jersey", "United States"), 2012542121);
         Employee employee3 = new Employee(3, "Paulie", "Gaultieri", "Underboss",
-                new Address(14, "Ohhhhh Boulevard", "Clifton", 07512, "New Jersey", "United States"), 201 - 235 - 3452);
+                new Address(14, "Ohhhhh Boulevard", "Clifton", 07512, "New Jersey", "United States"), 2012353452);
         Employee employee4 = new Employee(4, "Christopher", "Moltisanti", "Captain",
-                new Address(14, "Heroin Drive", "East Brunswick", 07121, "New Jersey", "United States"), 201 - 245 - 1256);
+                new Address(14, "Heroin Drive", "East Brunswick", 07121, "New Jersey", "United States"), 2012451256);
         Employee employee5 = new Employee(5, "Bobby", "Baccalieri", "Underboss",
-                new Address(14, "Food-Court Str", "Harrison", 07221, "New Jersey", "United States"), 201 - 243 - 5367);
+                new Address(14, "Food-Court Str", "Harrison", 07221, "New Jersey", "United States"), 2012435367);
         Employee employee6 = new Employee(6, "Ralph", "Cifaretto", "Captain",
-                new Address(14, "Gladiator Place", "North Bergen", 07331, "New Jersey", "United States"), 201 - 242 - 2355);
+                new Address(14, "Gladiator Place", "North Bergen", 07331, "New Jersey", "United States"), 2012422355);
         Employee employee7 = new Employee(7, "Vito", "Spatafore", "Captain",
-                new Address(14, "In The Closet Avenue", "Newark", 075161, "New Jersey", "United States"), 201 - 241 - 9824);
+                new Address(14, "In The Closet Avenue", "Newark", 075161, "New Jersey", "United States"), 201419824);
 
         employeesList.add(employee1);
         employeesList.add(employee2);
